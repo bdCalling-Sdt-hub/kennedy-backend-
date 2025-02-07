d11615e38d7d9f557977f822f2f434dfb60fe26c
Abir-Al-Arafat <antorabir@gmail.com>
2025-02-07 22:53:07 +0600

    update: subscription data

--- model/subscriptionPlan.model.js
+++ model/subscriptionPlan.model.js
@@ -16,6 +16,24 @@ const subscriptionPlanSchema = new mongoose.Schema(
       type: Number, // Duration in days (e.g., 30 for a monthly plan)
       required: true,
     },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["paid", "unpaid"],
+      default: "unpaid",
+    },
+    paymentId: {
+      type: String,
+    },
+    startDate: {
+      type: Date,
+    },
+    endDate: {
+      type: Date,
+    },
     features: {
       type: [String], // List of features included in the plan
       default: [],
