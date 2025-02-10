29f8900dba72510f20523d296fd2ac8c2725f070
abir <antorabir@gmail.com>
2025-02-10 13:36:50 +0600

    update: email content fixed

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -43,8 +43,16 @@ const signup = async (req, res) => {
         email: emailCheck.email,
         subject: "Account Activation Email",
         html: `
-                      <h6>Hello, ${emailCheck?.name || "User"}</h6>
-                      <p>Your email verification code is <h3>${emailVerifyCode}</h3> to verify your email</p>
+                      <div style="max-width: 500px; background: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); text-align: center; font-family: Arial, sans-serif;">
+      <h6 style="font-size: 16px; color: #333;">Hello, ${
+        emailCheck?.name || "User"
+      }</h6>
+      <p style="font-size: 14px; color: #555;">Your email verification code is:</p>
+      <div style="font-size: 24px; font-weight: bold; color: #d32f2f; background: #f8d7da; display: inline-block; padding: 10px 20px; border-radius: 5px; margin-top: 10px;">
+        ${emailVerifyCode}
+      </div>
+      <p style="font-size: 14px; color: #555;">Please use this code to verify your email.</p>
+    </div>
                       
                     `,
       };
